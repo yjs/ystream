8be80c6884b5e5b94d083df7cdc460c532a1c26f
Kevin Jahns <kevin.jahns@protonmail.com>
2022-12-08 12:57:52 +0100

    bindydoc

--- src/bindydoc.js
+++ src/bindydoc.js
@@ -1,5 +1,8 @@
 
+import * as Y from 'yjs'
 import * as bc from 'lib0/broadcastchannel'
+import * as buffer from 'lib0/buffer'
+import * as env from 'lib0/environment'
 
 /**
  * @typedef {import('./index.js').Ydb} Ydb
@@ -7,8 +10,38 @@ import * as bc from 'lib0/broadcastchannel'
 
 /**
  * @param {Ydb} ydb
- * @param {string}
+ * @param {string} collection
+ * @param {string} doc
+ * @param {Y.Doc} ydoc - should be an empty doc
  */
-export const bind = (ydb) => {
-  
+export const bindydoc = async (ydb, collection, doc, ydoc) => {
+  const bcroom = `${ydb.dbname}#${collection}#${doc}`
+  ydoc.on('updateV2', /** @type {function(Uint8Array, any)} */ (update, origin) => {
+    if (origin !== ydb) {
+      if (env.isBrowser) {
+        // @todo could use more efficient encoding - allow Uint8Array in lib0/bc
+        bc.publish(bcroom, buffer.toBase64(update), origin)
+      } else {
+        // @todo iterate through opened documents in ydb and apply update
+      }
+      ydb.addUpdate(collection, doc, update)
+    }
+  })
+  if (env.isBrowser) {
+    const sub = bc.subscribe(bcroom, (data, origin) => {
+      if (origin !== ydb) {
+        Y.applyUpdateV2(ydoc, buffer.fromBase64(data), ydb)
+      }
+    })
+    ydoc.on('destroy', () => {
+      bc.unsubscribe(bcroom, sub)
+    })
+  }
+  const updates = await ydb.getUpdates(collection, doc, 0)
+  Y.transact(ydoc, () => {
+    updates.forEach(update => {
+      Y.applyUpdateV2(ydoc, update.op.update)
+    })
+  }, ydb, false)
+  // @todo add ydoc to ydb.collections
 }
