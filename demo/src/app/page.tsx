132164291dc4b1c868e53d7cf61d38d5c62b662b
Kevin Jahns <kevin.jahns@protonmail.com>
2024-05-31 00:41:56 +0200

    prep

--- demo/src/app/page.tsx
+++ demo/src/app/page.tsx
@@ -1,22 +1,72 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import CodeMirror from '@uiw/react-codemirror'
 import * as Y from 'yjs'
 import { yCollab } from 'y-codemirror.next'
 import { javascript } from '@codemirror/lang-javascript'
 import Filetree from './components/filetree'
+import { WebsocketProvider } from 'y-websocket'
+import { WebrtcProvider } from 'y-webrtc'
+import { IndexeddbPersistence } from 'y-indexeddb'
+import ystream from './ystream'
 
-function Editor ({ ydoc }: { ydoc: Y.Doc | null }) {
-  if (ydoc === null) return <div />
-  return (
-    <CodeMirror extensions={[javascript(), yCollab(ydoc.getText(), null)]}/>
-  )
-}
+const ydoc = new Y.Doc()
+
+ystream.then(({ y, ycollection }) => {
+  ycollection.bindYdoc('root', ydoc)
+  y.transact(async tr => {
+    await ycollection.setFileInfo(tr, 'root', 'root.md', null, 'text')
+  })
+})
 
 export default function Home () {
-  const [ydoc, setYdoc] = useState(null as Y.Doc | null)
-  return <div id="main">
+  const [ydoc, setYdoc] = useState(null as null | Y.Doc)
+  return <div id="main">x
     <Filetree onDocSelection={setYdoc}/>
-    <Editor ydoc={ydoc}/>
+    {ydoc != null ? <CodeMirror extensions={[javascript(), yCollab(ydoc.getText(), null)]}/> : <div />}
   </div>
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -- copy paste in case of struggle..
+//
+// export default function Home () {
+//   const [ydoc, setYdoc] = useState(null as null | Y.Doc)
+//   return <div id="main">
+//     <Filetree onDocSelection={setYdoc}/>
+//     {ydoc != null ? <CodeMirror extensions={[javascript(), yCollab(ydoc.getText(), null)]}/> : <div />}
+//   </div>
+// }
--- demo/src/app/ystream.tsx
+++ demo/src/app/ystream.tsx
@@ -9,17 +9,18 @@ import * as buffer from 'lib0/buffer'
 
 const initYstream = async (): Promise<{ y: Ystream.Ystream, ycollection: Ystream.Collection }> => {
   if (typeof window === 'undefined') { return {} as any }
+  const testServerUser = 'AMgBeyJrZXlfb3BzIjpbInZlcmlmeSJdLCJleHQiOnRydWUsImt0eSI6IkVDIiwieCI6IkNZd01ha3BuMG9uYU5lQ2Etd3FMbjRGenNyaXNfVVk0WjVnUlFVQTl4UU9vaDk0WUc5T0hoSXRyNnJvdmFZcFoiLCJ5IjoiNzRVbGp1ODZJVU1KWnNZc1NqeFNqdXNMamo5VTZyb3pad2JLOVhhcWozTWdJV3Ruak55akwxRC1Oek9QM0ZKNyIsImNydiI6IlAtMzg0In0='
   const testUserRaw = {
     privateKey: '{"key_ops":["sign"],"ext":true,"kty":"EC","x":"pAUmLYc-UFmPIt7leafPTbhxQyygcaW7__nPcUNCuu0wH27yS9P_pWFP1GwcsoAN","y":"u3109KjrPGsNUn2k5Whn2uHLAckQPdLNqtM4GpBEpUJwlvVDvk71-lS3YOEYJ_Sq","crv":"P-384","d":"OHnRw5an9hlSqSKg966lFRvB7dow669pVSn7sFZUi7UQh_Y9Xc95SQ6pEWsofsYD"}',
     user: 'AMgBeyJrZXlfb3BzIjpbInZlcmlmeSJdLCJleHQiOnRydWUsImt0eSI6IkVDIiwieCI6InBBVW1MWWMtVUZtUEl0N2xlYWZQVGJoeFF5eWdjYVc3X19uUGNVTkN1dTB3SDI3eVM5UF9wV0ZQMUd3Y3NvQU4iLCJ5IjoidTMxMDlLanJQR3NOVW4yazVXaG4ydUhMQWNrUVBkTE5xdE00R3BCRXBVSndsdlZEdms3MS1sUzNZT0VZSl9TcSIsImNydiI6IlAtMzg0In0='
   }
-  const testServerUser = 'AMgBeyJrZXlfb3BzIjpbInZlcmlmeSJdLCJleHQiOnRydWUsImt0eSI6IkVDIiwieCI6IkNZd01ha3BuMG9uYU5lQ2Etd3FMbjRGenNyaXNfVVk0WjVnUlFVQTl4UU9vaDk0WUc5T0hoSXRyNnJvdmFZcFoiLCJ5IjoiNzRVbGp1ODZJVU1KWnNZc1NqeFNqdXNMamo5VTZyb3pad2JLOVhhcWozTWdJV3Ruak55akwxRC1Oek9QM0ZKNyIsImNydiI6IlAtMzg0In0='
   const testUser = {
     privateKey: await ecdsa.importKeyJwk(json.parse(testUserRaw.privateKey)),
     user: dbtypes.UserIdentity.decode(decoding.createDecoder(buffer.fromBase64(testUserRaw.user)))
   }
   const owner = buffer.toBase64(testUser.user.hash)
-  const collectionName = 'test'
+  const collectionName = 'lf-xx'
+
   const y: Ystream.Ystream = await Ystream.open('.ystream', {
     comms: [new wscomm.WebSocketComm('wss://ystream.yjs.dev', { owner, name: collectionName })]
   })
